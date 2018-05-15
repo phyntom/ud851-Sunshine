0e514e7d1ee83140594ba226ebd04c543bb8122c
Aimable Ruhumuriza <phyntom@gmail.com>
2018-05-15 06:37:23 +0200

    Modifiying the name of the dummy data array

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         textViewWeatherData = (TextView) findViewById(R.id.tv_weather_data);
 
         // COMPLETED (3) Create an array of Strings that contain fake weather data
-        String[] weatherData = {"Today, May 17 - Clear - 17°C / 15°C",
+        String[] dummyData = {"Today, May 17 - Clear - 17°C / 15°C",
                 "Tomorrow - Cloudy - 19°C / 15°C",
                 "Thursday - Rainy- 30°C / 11°C",
                 "Friday - Thunderstorms - 21°C / 9°C",
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 "Mon, May 30 - Post Apocalypse - 15°C / 10°C"};
 
         // TODO (4) Append each String from the fake weather data array to the TextView
-        for (String strData : weatherData) {
+        for (String strData : dummyData) {
             textViewWeatherData.append(strData + "\n\n");
         }
     }
